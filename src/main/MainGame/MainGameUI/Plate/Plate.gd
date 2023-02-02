42f33b6fae9ffd408feaf1b7c92a8406df44bae7
DeanNevan <920780950@qq.com>
2023-02-02 23:43:49 +0800

    美化表盘和ui

--- src/main/MainGame/MainGameUI/Plate/Plate.gd
+++ src/main/MainGame/MainGameUI/Plate/Plate.gd
@@ -2,9 +2,12 @@ extends MarginContainer
 
 @onready var _Plate = %Plate
 @onready var _Pointer = %Pointer
+@onready var _Panel = %Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	await get_tree().process_frame
+	_on_resized()
 	pass # Replace with function body.
 
 
@@ -15,6 +18,9 @@ func _process(delta):
 
 
 func _on_resized():
-	_Plate.pivot_offset = _Plate.size / 2
-	_Pointer.pivot_offset = _Pointer.size / 2
+	if is_instance_valid(_Plate) and is_instance_valid(_Pointer) and is_instance_valid(_Panel):
+		_Panel.pivot_offset = _Panel.size / 2
+		_Plate.pivot_offset = _Plate.size / 2
+		_Pointer.pivot_offset = _Pointer.size / 2
+		print(_Plate.size)
 	pass # Replace with function body.
