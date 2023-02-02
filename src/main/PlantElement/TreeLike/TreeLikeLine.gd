079e996b26bcc864c3b32e4c992930eecbf03f37
DeanNevan <920780950@qq.com>
2023-02-02 13:35:01 +0800

    在Test.tscn里写画笔功能

--- src/main/PlantElement/TreeLike/TreeLikeLine.gd
+++ src/main/PlantElement/TreeLike/TreeLikeLine.gd
@@ -1,15 +1,22 @@
-extends Node2D
+extends Line2D
 class_name TreeLikeLine
 
-var points := []
+enum Status {
+	BUILDING, 
+	NORMAL
+}
+var status : Status = Status.BUILDING
 
 @onready var _SubLines = %SubLines
 @onready var _AreaLine = %AreaLine
 @onready var _LineTexture = %LineTexture
-@onready var _PolygonTexture = %PolygonTexture
+@onready var _LineTextureBorder = %LineTextureBorder
+
+var parent_line : TreeLikeLine
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	default_color.a = 0
 	pass # Replace with function body.
 
 
@@ -17,7 +24,32 @@ func _ready():
 func _process(delta):
 	pass
 
-func init_texture_line():
+func set_status(_status : Status):
+	status = _status
+	if status == Status.BUILDING:
+		modulate.a = 0.5
+	elif status == Status.NORMAL:
+		modulate.a = 1
+
+func set_points(_points : PackedVector2Array):
+	points = _points
+	update_line_texture()
+
+func begin_build():
+	set_status(Status.BUILDING)
+	_AreaLine.monitorable = false
+	_AreaLine.monitoring = false
+
+func finish_build():
+	set_status(Status.NORMAL)
+	_AreaLine.monitorable = true
+	_AreaLine.monitoring = true
+	update_line_texture()
+	init_line_area()
+
+func update_line_texture():
+	_LineTexture.points = points
+	_LineTextureBorder.points = points
 	pass
 
 func init_line_area():
@@ -33,6 +65,7 @@ func init_line_area():
 
 func add_sub_line(sub_line : TreeLikeLine):
 	_SubLines.add_child(sub_line)
+	sub_line.parent_line = self
 
 func get_sub_lines() -> Array:
 	return _SubLines.get_children()
--- src/test/Test.gd
+++ src/test/Test.gd
@@ -1,20 +1,71 @@
 extends Node2D
 
+var drawing_line_parent : TreeLikeLine
+var drawing_line : TreeLikeLine
+var is_drawing := false
+
+var areas := []
+var tree_like_lines_shapes := {}
+
+@onready var _AreaMouseDrawer = %AreaMouseDrawer
+
+func _ready():
+	$Roots/TreeLikeLines/RootsLine.finish_build()
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_mask == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				request_start_draw_roots_line()
+			else:
+				request_stop_draw_roots_line()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_AreaMouseDrawer.global_position = get_global_mouse_position()
 	pass
 
+func request_start_draw_roots_line():
+	var closet_distance : float = 999
+	drawing_line_parent = null
+	if is_instance_valid(drawing_line):
+		drawing_line.queue_free()
+	for i in tree_like_lines_shapes:
+		if i is RootsLine:
+			for j in tree_like_lines_shapes[i]:
+				var segment_shape : SegmentShape2D = tree_like_lines_shapes[i][j]
+				var closed_point = Geometry2D.get_closest_point_to_segment(
+					get_global_mouse_position(),
+					segment_shape.a,
+					segment_shape.b
+				)
+	pass
 
-func _on_area_2d_mouse_entered():
-	print("!!!")
+func request_stop_draw_roots_line():
+	pass
+
+func _on_area_mouse_drawer_area_entered(area):
+	areas.append(area)
 	pass # Replace with function body.
 
 
-func _on_area_2d_mouse_exited():
-	print("???")
+func _on_area_mouse_drawer_area_exited(area):
+	areas.erase(area)
 	pass # Replace with function body.
 
 
-func _on_area_2d_area_entered(area):
-	print(area)
+func _on_area_mouse_drawer_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	if area is DetectArea2D:
+		if area.object is TreeLikeLine:
+			if tree_like_lines_shapes.get(area.object) == null:
+				tree_like_lines_shapes[area.object] = []
+			tree_like_lines_shapes[area.object].append(area.get_child(area_shape_index))
+	pass # Replace with function body.
+
+
+func _on_area_mouse_drawer_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	if area is DetectArea2D:
+		if area.object is TreeLikeLine:
+			if tree_like_lines_shapes.get(area.object) != null:
+				tree_like_lines_shapes[area.object].erase(area.get_child(area_shape_index))
 	pass # Replace with function body.
