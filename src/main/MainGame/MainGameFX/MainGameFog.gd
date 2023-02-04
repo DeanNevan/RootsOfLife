22ef71eb30482b25493d8cf9eef0e104797b1324
Abadunala <75767374+Abadunala@users.noreply.github.com>
2023-02-04 22:33:05 +0800

    v3

--- src/main/MainGame/MainGameFX/MainGameFog.gd
+++ src/main/MainGame/MainGameFX/MainGameFog.gd
@@ -1,32 +1,30 @@
 extends SubViewport
 
 
-@export var initial_visibility_path : NodePath
-var initial_visibility : Polygon2D
+@export var tilemap_path : NodePath
+var tilemap : TileMap
+@export var texturerect_path : NodePath
+var texturerect : TextureRect
 @export var following_camera_path : NodePath
 var following_camera : Camera2D
 @onready var camera = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initial_visibility = get_node(initial_visibility_path)
+	tilemap = get_node(tilemap_path)
+	texturerect = get_node(texturerect_path)
 	following_camera = get_node(following_camera_path)
 	get_parent().get_viewport().connect("size_changed", _on_viewport_size_changed)
 	_on_viewport_size_changed()
 	
-	if is_instance_valid(initial_visibility):
-		var new_line = $Line2D
-		new_line.position = initial_visibility.position
-		var points = initial_visibility.polygon
-		points.append(points[0])
-		new_line.points = points
-		self.add_child(new_line)
-		$Line2D.width = Global.fog_thickness
+	if is_instance_valid(tilemap) and is_instance_valid(texturerect):
+		var vp_tex = self.get_texture()
+		texturerect.texture = vp_tex
 
 func _on_viewport_size_changed():
-	size = get_parent().get_viewport().size
+	size = get_parent().get_viewport().size * 0.25
 
 func _process(_delta):
 	if is_instance_valid(following_camera):
 		camera.position = following_camera.position
-		camera.zoom = following_camera.zoom
+		camera.zoom = following_camera.zoom * 0.25
